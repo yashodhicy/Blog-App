4acc029ae1e987486b9889cf6747356d630fcab8
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-18 12:16:52 +0300

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates_presence_of :title
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
   after_create :update_user_post_count
   after_create :recent_comments
 

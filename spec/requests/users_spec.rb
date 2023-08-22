713544307217d2baaca5538f0fbf65d9da2613cd
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-22 13:14:04 +0300

    Fix linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,35 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "users", type: :request do
-  describe "GET /index" do
-    before (:example) {get '/users'}
-    
-    it "displays a list of users" do
+RSpec.describe 'users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/users' }
+
+    it 'displays a list of users' do
       expect(response).to have_http_status(200)
     end
 
-    it "renders the correct template" do
+    it 'renders the correct template' do
       expect(response).to render_template('index')
     end
 
-    it "includes correct placeholder text in the response body" do
+    it 'includes correct placeholder text in the response body' do
       expect(response.body).to include('Here is a list of users')
     end
 
-    describe "GET /show", type: :request do
-      before (:example) {get '/users/1'}
-      it "displays the user details for a given user" do
+    describe 'GET /show', type: :request do
+      before(:example) { get '/users/1' }
+      it 'displays the user details for a given user' do
         expect(response).to have_http_status(200)
       end
-  
-      it "renders the correct template" do
+
+      it 'renders the correct template' do
         expect(response).to render_template('show')
       end
-  
-      it "includes correct placeholder text in the response body" do
+
+      it 'includes correct placeholder text in the response body' do
         expect(response.body).to include('here is the user details')
       end
     end
   end
 end
-

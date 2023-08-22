e23584f8f1c86f7b68a9dc4ea8b7de82749e8c6c
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-22 13:11:19 +0300

    Add tests for controllers

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    before (:example) {get '/users/1/posts'}
+    
+    it "displays a list of posts" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the correct template" do
+      expect(response).to render_template('index')
+    end
+
+    it "includes correct placeholder text in the response body" do
+      expect(response.body).to include('Here is a list of posts')
+    end
+
+    describe "GET /show", type: :request do
+      before (:example) {get '/users/1/posts/1'}
+      it "displays the post details for a given post" do
+        expect(response).to have_http_status(200)
+      end
+  
+      it "renders the correct template" do
+        expect(response).to render_template('show')
+      end
+  
+      it "includes correct placeholder text in the response body" do
+        expect(response.body).to include('Here is a list of posts for a given user')
+      end
+    end
+  end
+end

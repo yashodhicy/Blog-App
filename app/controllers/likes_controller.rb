22d16d4ddac4b26120d273d3d2af2da26446251d
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-25 08:50:04 +0300

    Add like to posts

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,17 @@
+class LikesController < ApplicationController
+  def create
+    @current = current_user
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.build(user_id: @current.id)
+
+    respond_to do |format|
+      format.html do
+        if @like.save
+          redirect_to user_post_path(current_user, @post), notice: 'you liked the post' 
+        else
+          redirect_to user_post_path(current_user, @post), alert: 'unable to like'
+        end
+      end
+    end
+  end
+end

e1d13f26602ede999b52cd9351427ee67a0982a6
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-31 09:32:56 +0300

    Install devise to application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
@@ -11,4 +15,5 @@ class User < ApplicationRecord
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
+  
 end

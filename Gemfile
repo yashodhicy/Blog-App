5b48682270d795b642a99b0c053d86b18493412b
Sumeya <sumeyaibrahim72@gmail.com>
2023-08-31 12:21:10 +0300

    Fix linters

--- Gemfile
+++ Gemfile
@@ -78,4 +78,4 @@ group :test do
   gem 'selenium-webdriver'
 end
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,18 @@
 class ApplicationController < ActionController::Base
-  
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
-  
+
 
 
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :posts_counter, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :bio, :posts_counter, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :posts_counter, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :posts_counter, :email, :password, :current_password)
+    end
   end
 
   # private
@@ -24,5 +25,4 @@ class ApplicationController < ActionController::Base
   #   # Customize the path you want to redirect to after successful sign-in
   #   users_path
   # end
-
 end

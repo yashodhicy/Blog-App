cf001a53058ed6785dbbb6355d5cb7ebe0df8360
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-31 15:02:50 +0300

    Remove except index from user controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
   def index
     @users = User.all.order(id: :asc)
   end

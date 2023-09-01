78dd3579e63e1d6cd0a7d86076b6cb773209a946
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-09-01 18:18:29 +0300

    Uncomment authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
-
   before_action :load_user_abilities
 
   private
